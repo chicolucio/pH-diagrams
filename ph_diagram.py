59adeacf8ad10140b57766149423568755147dc0
Francisco Bustamante <chicolucio@gmail.com>
2021-05-13 14:54:58 -0300

    Codigo com classe Acid

--- ph_diagram.py
+++ ph_diagram.py
@@ -1,15 +1,27 @@
-import numpy as np 
+import numpy as np
 import matplotlib.pyplot as plt
 
 
 pH = np.arange(0, 14.1, 0.1)
 hydronium_concentration = 10**(-pH)
-pKa = 4.76
-Ka = 10**(-pKa)
 
-alpha_HA = hydronium_concentration / (hydronium_concentration + Ka)
-alpha_A = Ka / (hydronium_concentration + Ka)
 
-plt.plot(pH, alpha_HA)
-plt.plot(pH, alpha_A)
-plt.show()
+class Acid:
+    def __init__(self, pKa):
+        self.pKa = pKa
+        self.Ka = 10**(-pKa)
+
+    def alpha(self):
+        alpha_HA = hydronium_concentration / (hydronium_concentration + self.Ka)
+        alpha_A = self.Ka / (hydronium_concentration + self.Ka)
+        return alpha_HA, alpha_A
+
+    def distribution_diagram(self):
+        plt.plot(pH, self.alpha()[0])
+        plt.plot(pH, self.alpha()[1])
+        plt.show()
+
+
+if __name__ == "__main__":
+    acetic_acid = Acid(4.76)
+    acetic_acid.distribution_diagram()
