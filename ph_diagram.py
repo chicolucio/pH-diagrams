40af5243ca06234a558ac9b4aa334bcd2e4717de
Francisco Bustamante <chicolucio@gmail.com>
2021-05-18 15:37:31 -0300

    alpha e log_concentrations como properties

--- ph_diagram.py
+++ ph_diagram.py
@@ -15,14 +15,16 @@ class Acid:
         self.Ka = 10**(-pKa)
         self.Ca = acid_concetration
 
+    @property
     def alpha(self):
         alpha_HA = hydronium_concentration / (hydronium_concentration + self.Ka)
         alpha_A = self.Ka / (hydronium_concentration + self.Ka)
         return alpha_HA, alpha_A
 
+    @property
     def log_concentrations(self):
-        log10_HA = np.log10(self.alpha()[0] * self.Ca)
-        log10_A = np.log10(self.alpha()[1] * self.Ca)
+        log10_HA = np.log10(self.alpha[0] * self.Ca)
+        log10_A = np.log10(self.alpha[1] * self.Ca)
         return log10_HA, log10_A
 
     def plot_params(self, axis=None):
@@ -40,16 +42,16 @@ class Acid:
 
     def distribution_diagram(self):
         self.plot_params()
-        plt.plot(pH, self.alpha()[0])
-        plt.plot(pH, self.alpha()[1])
+        plt.plot(pH, self.alpha[0])
+        plt.plot(pH, self.alpha[1])
         plt.show()
 
     def pC_diagram(self):
         self.plot_params()
         plt.plot(pH, -pH)
         plt.plot(pH, -pOH)
-        plt.plot(pH, self.log_concentrations()[0])
-        plt.plot(pH, self.log_concentrations()[1])
+        plt.plot(pH, self.log_concentrations[0])
+        plt.plot(pH, self.log_concentrations[1])
         plt.show()
 
 
