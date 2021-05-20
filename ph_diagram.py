a7890575e620427d7cb3f151f2a5f4dd264241e5
Francisco Bustamante <chicolucio@gmail.com>
2021-05-20 15:20:29 -0300

    Metodo alpha em forma geral

--- ph_diagram.py
+++ ph_diagram.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from operator import mul
+from functools import reduce
 
 pH = np.arange(0, 14.1, 0.1)
 hydronium_concentration = 10**(-pH)
@@ -11,15 +12,17 @@ hydroxide_concentration = 10**(-pOH)
 
 class Acid:
     def __init__(self, pKa, acid_concetration):
-        self.pKa = pKa
-        self.Ka = 10**(-pKa)
+        self.pKa = np.array(pKa)
+        self.Ka = 10**(-self.pKa)
         self.Ca = acid_concetration
 
     @property
     def alpha(self):
-        alpha_HA = hydronium_concentration / (hydronium_concentration + self.Ka)
-        alpha_A = self.Ka / (hydronium_concentration + self.Ka)
-        return alpha_HA, alpha_A
+        numerators = []
+        for i in range(0, self.pKa.size + 1):
+            numerators.append(reduce(mul, self.Ka[0:i], hydronium_concentration**(self.pKa.size-i)))
+        alphas = [numerator/sum(numerators) for numerator in numerators]
+        return alphas
 
     @property
     def log_concentrations(self):
@@ -37,13 +40,14 @@ class Acid:
         ax.grid(b=True, axis='both', which='minor', linestyle=':', linewidth=1.0)
         ax.tick_params(axis='both', labelsize=14, length=6, which='major', width=1.5)
         ax.set_xlabel(r'pH', fontsize=16)
-        ax.set_ylabel(r'logc', fontsize=16)
+        # ax.set_ylabel(r'logc', fontsize=16)  # TODO consertar, não é esse label para distribuicao
         ax.set_axisbelow(True)
 
     def distribution_diagram(self):
         self.plot_params()
-        plt.plot(pH, self.alpha[0])
-        plt.plot(pH, self.alpha[1])
+        for alpha in self.alpha:
+            plt.plot(pH, alpha)
+            # TODO labels
         plt.show()
 
     def pC_diagram(self):
@@ -56,6 +60,17 @@ class Acid:
 
 
 if __name__ == "__main__":
-    acetic_acid = Acid(4.76, 0.1)
+    acetic_acid = Acid([4.76], 0.1)
     acetic_acid.distribution_diagram()
-    acetic_acid.pC_diagram()
+    # acetic_acid.pC_diagram()
+    fumaric_acid = Acid((3.02, 4.48), 0.1)  # figure 10.3 Harris
+    fumaric_acid.distribution_diagram()
+
+    # http://ion.chem.usu.edu/~sbialkow/Classes/3600/Overheads/H3A/H3A.html
+    tyrosine = Acid((2.17, 9.19, 10.47), 0.1)  # exercise 10.34 Harris
+    tyrosine.distribution_diagram()
+
+    CrIII_hydrolysis = (10**(-3.80), 10**(-6.40), 10**(-6.40), 10**(-11.40))
+    CrIII_pKa = np.log10(CrIII_hydrolysis) * -1
+    CrIII = Acid(CrIII_pKa, 0.1)
+    CrIII.distribution_diagram()
