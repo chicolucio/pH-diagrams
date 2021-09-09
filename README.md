7762b5f783a52ebdc1a45085cddfdcc3e55458b1
Natasha Souza <natasha.oliveira98@gmail.com>
2021-09-09 13:15:58 -0300

    Primeira versão

---
+++ README.md
@@ -0,0 +1,45 @@
+# pH diagrams
+
+![pH diagrams](ph_diagrams.png)
+
+A simple script to plot pH diagrams. pH diagrams describes acid–base equilibria.
+pH-log c diagrams show the logarithmic dependence of concentration with pH.
+Species distribution diagrams show the degree of dissociation as a function of pH.
+
+# Installation and requirements
+
+This project relies mainly on [Matplotlib](https://matplotlib.org/) and [Numpy](https://numpy.org/).
+
+Just clone or download this repo. This is not a package yet. 
+
+# Contributing
+
+All contributions are welcome.
+
+**Issues**
+
+Feel free to submit issues regarding:
+
+- recommendations
+- more examples for the tutorial
+- enhancement requests and new useful features
+- code bugs
+
+**Pull requests**
+
+- before starting to work on your pull request, please submit an issue first
+- fork the repo
+- clone the project to your own machine
+- commit changes to your own branch
+- push your work back up to your fork
+- submit a pull request so that your changes can be reviewed
+
+# Testing
+
+Tests can be run in the top-level directory with the command `pytest -v tests/`.
+
+# Citing
+
+If you use this project in a scientific publication or in classes, please consider citing as
+
+F. L. S. Bustamante & H. B. Soares, pH diagrams, 2021 - Available at: https://github.com/helenabenevenuto/pH-diagrams
---
+++ ph_diagram.py
@@ -0,0 +1,99 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from operator import mul
+from functools import reduce
+import plotly.graph_objects as go
+
+pH = np.arange(0, 14.1, 0.1)
+hydronium_concentration = 10**(-pH)
+pKw = 14
+pOH = pKw - pH
+hydroxide_concentration = 10**(-pOH)
+
+
+class Acid:
+    def __init__(self, pKa, acid_concetration):
+        self.pKa = np.array(pKa, dtype=float)
+        self.Ka = 10**(-self.pKa)
+        self.Ca = acid_concetration
+
+    @property
+    def alpha(self):
+        numerators = []
+        for i in range(0, self.pKa.size + 1):
+            numerators.append(reduce(mul, self.Ka[0:i], hydronium_concentration**(self.pKa.size-i)))
+        alphas = [numerator/sum(numerators) for numerator in numerators]
+        return alphas
+
+    @property
+    def log_concentrations(self):
+        return [np.log10(alpha * self.Ca) for alpha in self.alpha]
+
+    def plot_params(self, ylabel, axis=None, xlabel='pH'):
+        if axis is None:
+            fig, ax = plt.subplots(nrows=1, ncols=1, tight_layout=True)
+        else:
+            ax = axis
+        ax.grid(b=True, axis='both', which='major', linestyle='--', linewidth=1.5)
+        ax.minorticks_on()
+        ax.grid(b=True, axis='both', which='minor', linestyle=':', linewidth=1.0)
+        ax.tick_params(axis='both', labelsize=14, length=6, which='major', width=1.5)
+        ax.set_xlabel(xlabel, fontsize=16)
+        ax.set_ylabel(ylabel, fontsize=16)
+        ax.set_axisbelow(True)
+
+    def formulas(self):
+        labels = []
+        number_of_species = len(self.alpha)
+        for i in range(1, number_of_species + 1):
+            idx = number_of_species - i
+            charge = number_of_species - (number_of_species + i - 1)
+            if charge == 0:
+                charge = ''
+            if idx == 0:
+                labels.append(f'$A^{{{charge}}}$')
+            if idx == 1:
+                labels.append(f'$HA^{{{charge}}}$')
+            else:
+                labels.append(f'$H_{{{idx}}}A^{{{charge}}}$')
+        return labels
+
+    def distribution_diagram(self):
+        self.plot_params(ylabel=r'$\alpha$')
+        labels = self.formulas()
+        for i, alpha in enumerate(self.alpha):
+            plt.plot(pH, alpha, label=labels[i])
+        plt.legend(fontsize=16, bbox_to_anchor=(1, 1))
+        plt.show()
+
+    def pC_diagram(self):
+        self.plot_params(ylabel=r'$\log c$')
+        plt.plot(pH, -pH, color='black', linestyle='--', label='pH')
+        plt.plot(pH, -pOH, color='black', linestyle='--', label='pOH')
+        labels = self.formulas()
+        for i, logc in enumerate(self.log_concentrations):
+            plt.plot(pH, logc, label=labels[i])
+        plt.ylim(-14, 0)
+        plt.legend(fontsize=16, bbox_to_anchor=(1, 1))
+        plt.show()
+
+    def distribution_diagram(self):
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=pH,
+                                 y=self.alpha,
+                                 mode='lines',
+                                 name='Nao sei ainda',
+                                 opacity=0.5
+                                 hovertemplate="%{x:.2f}, %{y:.2f}",
+                                ))
+        fig.show()
+
+    def pC_diagram(self):
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=pH,
+                                 y=self.log_concentrations,
+                                 mode='lines',
+                                 name='pH - log C',
+                                 hovertemplate="%{x:.2f}, %{y:.2f}",
+                                ))
+        fig.show()
